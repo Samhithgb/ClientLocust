270c6ade930bb9f5d92a327371fa20a99d58b28d
Samhith <sbarlaya@vmware.com>
2021-08-26 11:29:40 -0400

    Changed route

--- server.py
+++ server.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 from flask import send_file
 
 
-@app.route("/")
+@app.route("/download")
 def download():
     path = "Comatose-Servers-Redux-2017.pdf"
     return send_file(path, as_attachment=True)
 
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
